183e769e755ef97eb703d58d196537e767c3a65f
Brian Schirk <43097841+berschirker@users.noreply.github.com>
2021-11-03 13:23:08 -0400

    Update tsdl.ps1

--- tsdl.ps1
+++ tsdl.ps1
@@ -3,34 +3,33 @@ $url = "https://pkgs.tailscale.com/stable/tailscale-ipn-setup-1.16.2.exe"
 $output = "C:\Users\Public\tailscale-ipn-setup-1.16.2.exe"
 $start_time = Get-Date
 cls
-Write "This script will install Tailscale and remove Hamachi from this computer."
+Write "This program will install Tailscale and remove Hamachi from this computer."
 Start-Sleep -Seconds 3
 cls
 Write "Downloading Tailscale installer..."
 Invoke-WebRequest -Uri $url -OutFile $output
-Write "Download Complete"
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 Start-Sleep -Seconds 2
-cls
 cd C:\Users\Public\
-Write "Installing Tailscale"
 Start-Process -Wait -FilePath "C:\Users\Public\tailscale-ipn-setup-1.16.2.exe" -ArgumentList "/S" -PassThru
+cls
+Write "Installing Tailscale"
+cls
 Write "Tailscale has been installed successfully"
 Start-Sleep -Seconds 2
 Write "Cleaning Up"
-Start-Sleep -Seconds 2
-rm tsdldownloader.ps1
-rm tailscale-ipn-setup-1.16.2.exe
+Start-Sleep -Seconds 1
 rm tsdl.ps1
+rm tailscale-ipn-setup-1.16.2.exe
 cls
 Write "Uninstalling Hamachi..."
 get-package Logmein* | uninstall-package
+cls
 Write "Hamachi Uninstalled Successfully"
 Start-Sleep -Seconds 2
 cls
 Write "Please Log in to Tailscale"
-iex "tailscale up" -PassThru
+Start-Sleep -Seconds 2
 cls
 Write "Process Complete. Exiting..."
-Sleep -S 2
+Start-Sleep -Seconds 3
 Stop-Process -Id $PID
